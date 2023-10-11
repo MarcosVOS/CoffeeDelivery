5fe6dff48c0e3028a6ba91332da0d488362fd4de
MarcosVOS <Marcos.OliveiraDEV@gmail.com>
2023-10-11 01:39:49 -0300

    add a file with all colors

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  white: '#FFFFFF',
+  'white-200': '#FAFAFA',
+  'white-300': '#F3F2F2',
+  'white-400': '#EDEDED',
+  'white-500': '#E6E5E5',
+
+  'yellow-100': '#F1E9C9',
+  'yellow-200': '#DBAC2C',
+  'yellow-300': '#C47F17',
+
+  'brown-100': '#D7D5D5',
+  'brown-200': '#8D8686',
+  'brown-300': '#574F4D',
+  'brown-400': '#403937',
+  'brown-500': '#272221',
+
+  'purple-100': '#EBE5F9',
+  'purple-200': '#8047F8',
+  'purple-300': '#4B2995',
+}

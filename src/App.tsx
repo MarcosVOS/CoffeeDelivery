ad008249dc3894b8d379f439ccab6c859649e37f
MarcosVOS <Marcos.OliveiraDEV@gmail.com>
2023-10-11 01:57:57 -0300

    add global style

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
@@ -9,6 +10,7 @@ export function App() {
       <BrowserRouter>
         <Router />
       </BrowserRouter>
+      <GlobalStyle />
     </ThemeProvider>
   )
 }

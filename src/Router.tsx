4941b850ed95adf21984b7547cacc9e175acc716
MarcosVOS <Marcos.OliveiraDEV@gmail.com>
2023-10-11 02:06:43 -0300

    Create the `header` component

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,16 +2,19 @@ import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/home'
 import { Checkout } from './pages/checkout'
 import { PaymentConfirmation } from './pages/paymentconfirmation'
+import { DefaultLayout } from './layout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/checkout" element={<Checkout />}></Route>
-      <Route
-        path="/paymentconfirmation"
-        element={<PaymentConfirmation />}
-      ></Route>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/checkout" element={<Checkout />}></Route>
+        <Route
+          path="/paymentconfirmation"
+          element={<PaymentConfirmation />}
+        ></Route>
+      </Route>
     </Routes>
   )
 }

613f61f34700536f4c93a98065cb3bf1aa1bce7c
MarcosVOS <Marcos.OliveiraDEV@gmail.com>
2024-01-02 19:43:09 -0300

    add the entire main structure of the congratulations on your purchase page

--- src/pages/paymentconfirmation/index.tsx
+++ src/pages/paymentconfirmation/index.tsx
@@ -2,9 +2,11 @@ import {
   ConfirmationContainer,
   ConfirmationMessageAbout,
   ConfirmationMessageTitle,
+  InfoTitle,
   TitleAbout,
 } from './styles'
 import Illustration from '../../assets/Illustration.png'
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
 
 export function PaymentConfirmation() {
   return (
@@ -12,10 +14,31 @@ export function PaymentConfirmation() {
       <ConfirmationMessageTitle>
         <h1>Uhu! Pedido confirmado</h1>
         <span>Agora é só aguardar que logo o café chegará até você</span>
+        <ConfirmationMessageAbout>
+          <TitleAbout iconProps="purple200">
+            <MapPin size={32} weight="fill" />
+            <InfoTitle>
+              Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+              <br />
+              Farrapos - Porto Alegre, RS
+            </InfoTitle>
+          </TitleAbout>
+          <TitleAbout iconProps="yellow200">
+            <Timer size={32} weight="fill" />
+            <InfoTitle>
+              Previsão de entrega <br />
+              <strong> 20 min - 30 min</strong>
+            </InfoTitle>
+          </TitleAbout>
+          <TitleAbout iconProps="yellow300">
+            <CurrencyDollar size={32} weight="fill" />
+            <InfoTitle>
+              Pagamento na entrega <br /> <strong> Cartão de Crédito</strong>
+            </InfoTitle>
+          </TitleAbout>
+        </ConfirmationMessageAbout>
       </ConfirmationMessageTitle>
-      <ConfirmationMessageAbout>
-        <TitleAbout></TitleAbout>
-      </ConfirmationMessageAbout>
+
       <img src={Illustration} alt="" />
     </ConfirmationContainer>
   )
--- src/pages/paymentconfirmation/styles.ts
+++ src/pages/paymentconfirmation/styles.ts
@@ -1,11 +1,76 @@
 import styled from 'styled-components'
 
-export const ConfirmationContainer = styled.div``
+export const ConfirmationContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 5rem;
+  gap: 6.375rem;
 
-export const ConfirmationMessageTitle = styled.div``
+  img {
+    margin-top: 8.25rem;
+
+    width: 492px;
+    height: 293px;
+  }
+`
+
+export const ConfirmationMessageTitle = styled.div`
+  h1 {
+    color: ${(props) => props.theme['yellow-300']};
+    font-family: 'Baloo 2';
+    font-size: 2rem;
+  }
+
+  span {
+    font-size: 1.25rem;
+    font-weight: 400;
+    line-height: 130%;
+  }
+`
 
 export const ImageContainer = styled.div``
 
-export const ConfirmationMessageAbout = styled.div``
+export const ConfirmationMessageAbout = styled.div`
+  margin-top: 2.5rem;
 
-export const TitleAbout = styled.span``
+  display: flex;
+  flex-direction: column;
+  width: 32.875rem;
+  padding: 2.5rem;
+  gap: 2rem;
+
+  border-radius: 6px 36px;
+  border: 1px solid ${(props) => props.theme['yellow-200']};
+`
+
+const ICON_COLORS = {
+  yellow300: 'yellow-300',
+  yellow200: 'yellow-200',
+  purple200: 'purple-200',
+}
+interface IconProps {
+  iconProps: keyof typeof ICON_COLORS
+}
+
+export const TitleAbout = styled.div<IconProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+
+  svg {
+    padding: 8px;
+    color: white;
+    background-color: ${(props) => props.theme[ICON_COLORS[props.iconProps]]};
+    border-radius: 100%;
+
+    width: 32px;
+    height: 32px;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+`
+
+export const InfoTitle = styled.span`
+  flex-direction: column;
+`
